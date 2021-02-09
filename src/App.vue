2a9119b577e61663859a15d8d8ec49ca28ac19e6
Alesson Marques <alessonmarques@live.com>
2021-02-08 23:05:16 -0300

    prepearing to start programming

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,12 @@
   export default {
     computed:{
 
-      // user () {
-      //   const { name, email } = this.$store.state.user
-      //   return `Usuário ${name} e ${email}.`
-      // }
-
     }
   }
 </script>
 
 <template>
   <div>
-    <h2>teste</h2>
-    <!-- <h4>{{ user }}</h4> -->
+    <h2>start</h2>
   </div>
 </template>
