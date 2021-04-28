6315c52a036c6faf8609562e1a30aadcb83234ef
Alesson Marques <alessonmarques@live.com>
2021-04-28 01:30:56 -0300

    Removing get instagram data from the client.

--- src/features/brothers/store/actions.js
+++ src/features/brothers/store/actions.js
@@ -5,9 +5,10 @@ const setData = async ({ commit }, obj) => {
 
   const data = (await axios.get('http://k-nella.ddns.net:8080/bbb-abstract/getBigBrotherBrasilData.php')).data
 
-    await Promise.all(data.brothers.map(async (brother, key) => {
-      data.brothers[key].instagramData = ( await axios.get(brother.instagramUrl)).data.graphql.user
-    }));
+    //Removed 2021/04/28
+    // await Promise.all(data.brothers.map(async (brother, key) => {
+    //   data.brothers[key].instagramData = ( await axios.get(brother.instagramUrl)).data.graphql.user
+    // }));
 
   commit('SET_DATA', { data })
 
--- src/store/index.js
+++ src/store/index.js
@@ -4,7 +4,6 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 import Brothers from '../features/brothers/store'
-//import Brothers from '../features/brothers/store'
 
 const modules = {
   Brothers
